22fd3811bc2a2c772b9386070d1aca04965b5c74
kavin1001 <kavin.saravanan101@gmail.com>
2024-11-21 18:09:10 -0500

    Added changes in README:

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 #!/usr/local/bin/node
 
 const { v4: uuidv4 } = require('uuid');
+const os = require('os');
 
 console.log('Hello World: ' + uuidv4())
+console.log('Total system memory: ' + os.totalmem());
+console.log('Free system memory: ' + os.freemem());
