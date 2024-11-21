60d2cccbc6f0e5f9eff5590d9128e7d8df4029da
kavin1001 <kavin.saravanan101@gmail.com>
2024-11-21 18:13:32 -0500

    Fixed yaml file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/local/bin/node
 
-const { v4: uuidv4 } = require('uuid');
 const os = require('os');
+const { v4: uuidv4 } = require('uuid');
 
-console.log('Hello World: ' + uuidv4())
+console.log('Hello World: ' + uuidv4());
 console.log('Total system memory: ' + os.totalmem());
 console.log('Free system memory: ' + os.freemem());
